890070289ebc4fb3c2267db7237cb8dd835a6d6c
JuliusDgenius <ibejulius1@gmail.com>
2023-09-24 14:11:27 +0100

    header file

--- sort.h
+++ sort.h
@@ -7,15 +7,14 @@
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
  */
-
-#include <stdio.h>
-
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+const int n;
+struct listint_s *prev;
+struct listint_s *next;
 } listint_t;
+
+#include <stdio.h>
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
@@ -33,6 +32,6 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
-void sort_deck(deck_node_t **deck);
+
 
 #endif
