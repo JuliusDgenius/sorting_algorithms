73f8d66886908dec1d605e04978006f63f42d962
JuliusDgenius <ibejulius1@gmail.com>
2023-09-25 06:41:16 +0100

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,19 +2,6 @@
 #include <stdio.h>
 
 /**
- * swap - swaps values in an array
- * @a: first integer
- * @b: second integer
- * Return: void
- */
-void swap(int *a, int *b)
-{
-int tmp;
-tmp = *a;
-*a = *b;
-*b = tmp;
-}
-/**
  * bubble_sort - sorting function following the bubble sort algorithm
  * @array: Unsorted array
  * @size: Size of the array
@@ -22,7 +9,7 @@ tmp = *a;
  */
 void bubble_sort(int *array, size_t size)
 {
-size_t i, j;
+size_t i, j, tmp;
 
 for (i = 0; i < size - 1; i++)
 {
@@ -30,7 +17,9 @@ for (j = 0; j < size - i - 1; j++)
 {
 if (array[j] > array[j + 1])
 {
-swap(&array[j], &array[j + 1]);
+tmp = array[j];
+array[j] = array[j + 1];
+array[j + 1] = tmp;
 print_array(array, size);
 }
 }
