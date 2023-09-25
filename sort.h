92c1b4e4bae50dc283fec713d165fee0c2fbdf42
JuliusDgenius <ibejulius1@gmail.com>
2023-09-25 07:13:22 +0100

    sort header file

--- sort.h
+++ sort.h
@@ -20,7 +20,7 @@ struct listint_s *next;
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-void swap(int *a, int *b);
+void swap(listint_t *first_node, listint_t *second_node, listint_t **list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 listint_t *create_listint(const int *array, size_t size);
