8bacaa90fa0020c88fe8409a9eda57b78a407b7f
JuliusDgenius <ibejulius1@gmail.com>
2023-09-26 09:43:51 +0100

    correction

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -15,15 +15,12 @@ size_t i, k, m, n;
 
 if (size < 2)
 return;
-
 max = array[0];
 for (i = 1; i < size; i++)
 if (array[i] > max)
 max = array[i];
-
 count_arr = malloc(sizeof(size_t) * (max + 1));
 out_arr = malloc(sizeof(int) * size);
-
 for (j = 0; j <= max; j++)
 count_arr[j] = 0;
 for (k = 0; k < size; k++)
@@ -41,7 +38,6 @@ count_arr[array[m]]--;
 }
 for (n = 0; n < size; n++)
 array[n] = out_arr[n];
-
 free(count_arr);
 free(out_arr);
 }
